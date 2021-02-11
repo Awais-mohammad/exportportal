f63f157ea5b94590549ce8865f1db9b9d5c38a4f
awaisdev <muhammadawais083@gmail.com>
2021-02-12 00:07:42 +0500

    gotoadded

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
-
-  constructor(
-    private router: Router,
-  ) { }
-
   slideOpts = {
     grabCursor: true,
     initialSlide: 0,
@@ -174,9 +167,11 @@ export class HomePage {
     }
   }
 
-  goToPage(path: string) {
-    this.router.navigate([path]).then(() => {
-    });
+
+  goToPage(){
+
   }
+  
+  constructor() {}
 
 }
