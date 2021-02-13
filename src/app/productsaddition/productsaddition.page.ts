e29c7dbd10297c941a651b64baaad0a04c28045b
awaisdev <muhammadawais083@gmail.com>
2021-02-13 23:56:44 +0500

    fg

--- src/app/productsaddition/productsaddition.page.ts
+++ src/app/productsaddition/productsaddition.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-productsaddition',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsadditionPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fireStore: AngularFirestore,
+  ) { }
+
+  categories: any;
+  selectedcat: string;
+
+  temp: any;
+  subtypes: any[];
+
+  getCats() {
+    const cats = this.fireStore.collection('appData').doc('categories').valueChanges().subscribe((data: any) => {
+      this.categories = data;
+      this.temp = data
+      this.subtypes = this.categories.cats;
+      console.log('ksghoiw', this.subtypes);
+
+    })
+  }
+
+  choosecat(selested: string) {
+    this.selectedcat = selested
+    alert(this.selectedcat)
+    console.log(this.temp);
+    for (var i = 0; i < this.temp[selested].length; i++) {
+      console.log('in loop');
+
+    }
+
+  }
+
+
 
   ngOnInit() {
+    this.getCats()
   }
 
 }
