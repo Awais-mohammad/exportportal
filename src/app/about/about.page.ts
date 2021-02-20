174c4f6f074f5d532e7c06acdf695bbaaee261a5
awaisdev <muhammadawais083@gmail.com>
2021-02-20 21:25:28 +0500

    check

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AboutPage implements OnInit {
 
   constructor() { }
+  width = window.innerWidth;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.width = window.innerWidth;
+  }
+  
   ngOnInit() {
   }
 
