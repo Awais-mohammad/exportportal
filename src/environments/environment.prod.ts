127da4cf2e3b9084d644c13cb4b1d58c18d7be07
ekniazi <ekniazi@gmail.com>
2021-03-03 14:23:33 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbSi_LAYt9MKSP7FdSvEaOcYr4i1RWnCc",
+    authDomain: "exportportal-15e5e.firebaseapp.com",
+    projectId: "exportportal-15e5e",
+    storageBucket: "exportportal-15e5e.appspot.com",
+    messagingSenderId: "267169133080",
+    appId: "1:267169133080:web:266abf71b6b22cbf44096d",
+    measurementId: "G-B946MEV724"
+  }
 };
