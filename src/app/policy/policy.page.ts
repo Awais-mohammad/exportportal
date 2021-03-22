47e3ef02014a9be9eec44df6258866a7e1476f51
ekniazi <ekniazi@gmail.com>
2021-03-22 23:27:20 +0500

    Scroll and Hash adjust

--- src/app/policy/policy.page.ts
+++ src/app/policy/policy.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
+import { IonContent } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-policy',
   templateUrl: './policy.page.html',
   styleUrls: ['./policy.page.scss'],
 })
+
 export class PolicyPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonContent, { static: true }) content: IonContent;
+
+  constructor(private location: Location,) { }
 
   ngOnInit() {
+  }
+
+  scrollToTop() {
+    this.content.scrollToTop(1000);
+  }
+
+  
+  scrollToBot() {
+    this.content.scrollToBottom(1000);
+  }
+
+
+  ionViewWillEnter() {
+    window.location.href = "policy#Disclaimer";
+    this.content.scrollToTop(400);
   }
 
 }
